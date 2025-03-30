c3bf3fddbd5d2a26859d95768ac2b9636791e8cf
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2025-03-30 23:09:24 +0300

    create contact page

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,7 @@
 export function Contact() {
     const divElement = document.createElement("div");
     divElement.classList.add("contact-content")
-    divElement.style.cssText = ("display: flex; flex-direction: column align-items: strech;");
+    divElement.style.cssText = ("display: flex; flex-direction: column; align-items: strech; gap: 2rem;");
 
     const contentElement = document.querySelector("#content");
     contentElement.appendChild(divElement);
@@ -11,4 +11,25 @@ export function Contact() {
     h2Element.style.cssText = "text-align: center; font-size: 2.5rem";
     divElement.appendChild(h2Element);
 
+    const pElement = document.createElement("p");
+    pElement.textContent = "Have questions? Want to make a reservation? Or just want to chat about your favorite dish? We’re here to help!";
+    pElement.style.cssText = "text-align: center; font-size: 1.3rem; font-weight: normal";
+    divElement.appendChild(pElement);
+    
+    const ulElement = document.createElement("ul");
+    divElement.appendChild(ulElement);
+
+    const contactList = [
+        "Phone: +123456789",
+        "Email: contact@fat.chocobo",
+        "Address: Fat Chocobo’s Roost, Chocobo Sanctuary",
+    ]
+    contactList.forEach((item) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = item;
+        listItem.style.fontWeight = "normal";
+        listItem.style.fontSize = "2rem";
+        listItem.style.padding = "1rem";
+        ulElement.appendChild(listItem);
+    })
 }
